78bd75d380b00a9724e42c2ba0bbbf2491753640
ww-github <info@whitewater.guide>
2020-02-18 18:24:53 +0300

    fix: tag

--- publish.sh
+++ publish.sh
@@ -6,4 +6,5 @@ VERSION=$1
 
 docker login docker.pkg.github.com -u ${GITHUB_USER} -p ${GITHUB_TOKEN}
 docker push docker.pkg.github.com/whitewater-guide/pg_dump_restore/pg_dump_restore:latest
+docker tag docker.pkg.github.com/whitewater-guide/pg_dump_restore/pg_dump_restore:latest docker.pkg.github.com/whitewater-guide/pg_dump_restore/pg_dump_restore:${VERSION}
 docker push docker.pkg.github.com/whitewater-guide/pg_dump_restore/pg_dump_restore:${VERSION}
