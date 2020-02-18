ea4a6df2f7fc958e5dd2d6fa372a075c9ddd1a1d
ww-github <info@whitewater.guide>
2020-02-18 18:18:50 +0300

    fix: package name

--- publish.sh
+++ publish.sh
@@ -5,5 +5,5 @@ set -e
 VERSION=$1
 
 docker login docker.pkg.github.com -u ${GITHUB_USER} -p ${GITHUB_TOKEN}
-docker push docker.pkg.github.com/whitewater-guide/gorge/gorge:latest
-docker push docker.pkg.github.com/whitewater-guide/gorge/gorge:${VERSION}
+docker push docker.pkg.github.com/whitewater-guide/pg_dump_restore/pg_dump_restore:latest
+docker push docker.pkg.github.com/whitewater-guide/pg_dump_restore/pg_dump_restore:${VERSION}
