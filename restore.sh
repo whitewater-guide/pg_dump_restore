cfdeb3fa4c85ee9ad40138144085116c07a1c142
ww-github <info@whitewater.guide>
2020-02-20 23:32:46 +0300

    fix: revoke connections in restore too

--- restore.sh
+++ restore.sh
@@ -22,15 +22,21 @@ echo "Extracting backup contents"
 tar -xvf backup.tar
 
 echo "Restoring wwguide database..."
-pg_restore -h db -U postgres -d wwguide -Fc wwguide.bak || true
+psql -h db -U postgres -d wwguide -c "REVOKE CONNECT ON DATABASE wwguide FROM public;"
+psql -h db -U postgres -d wwguide -c "SELECT pid, pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = current_database() AND pid <> pg_backend_pid();"
+pg_restore -h db -U postgres -d wwguide -Fc --clean --create wwguide.bak  || true
+psql -h db -U postgres -d wwguide -c "GRANT CONNECT ON DATABASE wwguide TO public;"
 echo "Restored wwguide database"
 
 
 echo "Restoring gorge database..."
+psql -h db -U postgres -d gorge -c "REVOKE CONNECT ON DATABASE gorge FROM public;"
+psql -h db -U postgres -d gorge -c "SELECT pid, pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = current_database() AND pid <> pg_backend_pid();"
 psql -h db -U postgres -d gorge  -c "CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;"
 psql -h db -U postgres -d gorge  -c "SELECT timescaledb_pre_restore();"
 pg_restore -h db -U postgres -d wwguide -Fc gorge.bak  || true
 psql -h db -U postgres -d gorge  -c "SELECT timescaledb_post_restore();"
+psql -h db -U postgres -d gorge -c "GRANT CONNECT ON DATABASE gorge TO public;"
 echo "Restore complete"
 rm -rf *.bak *.csv *.tar
 echo "Deleted current backups"
