af1f0ed5ab75d0e83ad49afcf0a565e147dfb462
ww-github <info@whitewater.guide>
2020-03-01 18:44:00 +0300

    fix: attempt to fix restore

--- restore_partial.sh
+++ restore_partial.sh
@@ -21,21 +21,21 @@ echo "Extracting backup contents"
 tar -xvf partial.tar
 
 echo "Restoring wwguide database..."
-psql -h db -U postgres -d wwguide -c "REVOKE CONNECT ON DATABASE wwguide FROM public;"
-psql -h db -U postgres -d wwguide -c "SELECT pid, pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = current_database() AND pid <> pg_backend_pid();"
+psql -h db -U postgres -d postgres -c "REVOKE CONNECT ON DATABASE wwguide FROM public;"
+psql -h db -U postgres -d postgres -c "SELECT pid, pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = 'wwguide';"
 pg_restore -h db -U postgres -d wwguide -Fc --clean --create wwguide.bak  || true
-psql -h db -U postgres -d wwguide -c "GRANT CONNECT ON DATABASE wwguide TO public;"
+psql -h db -U postgres -d postgres -c "GRANT CONNECT ON DATABASE wwguide TO public;"
 echo "Restored wwguide database from ${LATEST_BACKUP}"
 
 echo "Restoring gorge database..."
-psql -h db -U postgres -d gorge -c "REVOKE CONNECT ON DATABASE gorge FROM public;"
-psql -h db -U postgres -d gorge -c "SELECT pid, pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = current_database() AND pid <> pg_backend_pid();"
+psql -h db -U postgres -d postgres -c "REVOKE CONNECT ON DATABASE gorge FROM public;"
+psql -h db -U postgres -d postgres -c "SELECT pid, pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = 'gorge';"
 pg_restore -h db -U postgres -d gorge -Fc --clean --create gorge_schema.bak  || true
 psql -h db -U postgres -d gorge -c "INSERT INTO schema_migrations (version, dirty) VALUES (1, false);"
 psql -h db -U postgres -d gorge -c "CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;"
 psql -h db -U postgres -d gorge -c "SELECT create_hypertable('measurements', 'timestamp');"
 psql -h db -U postgres -d gorge -c "\copy measurements FROM '/app/measurements.csv'"
-psql -h db -U postgres -d gorge -c "GRANT CONNECT ON DATABASE gorge TO public;"
+psql -h db -U postgres -d postgres -c "GRANT CONNECT ON DATABASE gorge TO public;"
 echo "Restore complete from ${LATEST_BACKUP}"
 rm -rf *.bak *.csv *.tar
 echo "Deleted current backups"
