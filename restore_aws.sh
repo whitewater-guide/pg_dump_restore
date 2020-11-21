2bbc6ee4579365e3024870648ab709c9357ee82c
ww-github <info@whitewater.guide>
2020-11-21 16:26:07 +0300

    fix: restore schema too in aws

--- restore_aws.sh
+++ restore_aws.sh
@@ -15,11 +15,11 @@ echo "Extracting backup contents"
 tar -xvf backup.tar
 
 echo "Restoring wwguide database..."
-pg_restore -d wwguide -Fc --data-only --disable-triggers --schema public wwguide.bak  || true
+pg_restore -d wwguide -Fc --schema public wwguide.bak || true
 echo "Restored wwguide database from ${LATEST_BACKUP}"
 
 echo "Restoring gorge database..."
-pg_restore -d gorge -Fc --data-only --disable-triggers --table measurements gorge.bak  || true
+pg_restore -d gorge -Fc --table measurements --table jobs gorge.bak || true
 echo "Restore complete from ${LATEST_BACKUP}"
 rm -rf *.bak *.csv *.tar
 echo "Deleted current backups"
