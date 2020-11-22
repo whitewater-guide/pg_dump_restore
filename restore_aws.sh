bc4610a7b49dcdf077fc5b39fdbedf13a82613d4
ww-github <info@whitewater.guide>
2020-11-22 22:08:11 +0300

    fix: improve aws messages

--- restore_aws.sh
+++ restore_aws.sh
@@ -13,10 +13,10 @@ tar -xvf backup.tar
 
 echo "Restoring wwguide database..."
 pg_restore -d wwguide -Fc --schema public wwguide.bak || true
-echo "Restored wwguide database from ${LATEST_BACKUP}"
+echo "Restored wwguide"
 
 echo "Restoring gorge database..."
 pg_restore -d gorge -Fc --table measurements --table jobs gorge.bak || true
-echo "Restore complete from ${LATEST_BACKUP}"
+echo "Restored gorge"
 rm -rf *.bak *.csv *.tar
 echo "Deleted current backups"
