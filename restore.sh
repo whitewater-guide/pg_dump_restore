8de9573f9aa17141d09a64f29ae04c8aec537eb1
doomsower <K.Kuznetcov@gmail.com>
2021-06-10 16:26:56 +0300

    fix: aws env vars

--- restore.sh
+++ restore.sh
@@ -3,11 +3,6 @@
 set -e
 set -o pipefail
 
-# env vars needed for aws tools
-export AWS_ACCESS_KEY_ID=$S3_ACCESS_KEY_ID
-export AWS_SECRET_ACCESS_KEY=$S3_SECRET_ACCESS_KEY
-export AWS_DEFAULT_REGION=$S3_REGION
-
 export PGPASSWORD=$POSTGRES_PASSWORD
 
 echo "Finding latest backup"
