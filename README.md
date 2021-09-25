3334944fee610a7b1307d159542fb6a38a788bab
doomsower <K.Kuznetcov@gmail.com>
2021-09-25 16:28:14 +0300

    feat: add option to skip dump partitions

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-This docker image is used to backup and restore whitewater.guide databases
+This docker image is used to backup and restore whitewater.guide databases.
 
 Available via https://github.com/orgs/whitewater-guide/packages/container/package/pg_dump_restore
 
@@ -10,3 +10,4 @@ Expected environment variables:
 - `PGUSER` - postgres user
 - `POSTGRES_PASSWORD` - postgres password, note different style (`POSTGRES_` not `PG`)
 - `SKIP_GORGE` - if set, won't restore gorge database
+- `SKIP_PARTITIONS` - if set, won't archive measurements partitions
--- backup.sh
+++ backup.sh
@@ -10,33 +10,36 @@ rm -rf *.bak *.csv *.tar *.tar.gz ./partitions
 
 mkdir ./partitions
 
-# Dump old measurements partitions
-# https://github.com/pgpartman/pg_partman/blob/master/doc/pg_partman.md#scripts
+if test -z "${SKIP_PARTITIONS}" 
+then
+    # Dump old measurements partitions
+    # https://github.com/pgpartman/pg_partman/blob/master/doc/pg_partman.md#scripts
 
-# First, with --nodrop to ensure that they're uploaded to s3
-echo "Dumping old measurements partitions with --nodrop"
-dump_partition.py \
-    --schema archive \
-    --connection "dbname=gorge host=${PGHOST} user=${PGUSER} password=${PGPASSWORD}" \
-    --output ./partitions \
-    --nodrop \
-    --dump_database gorge
+    # First, with --nodrop to ensure that they're uploaded to s3
+    echo "Dumping old measurements partitions with --nodrop"
+    dump_partition.py \
+        --schema archive \
+        --connection "dbname=gorge host=${PGHOST} user=${PGUSER} password=${PGPASSWORD}" \
+        --output ./partitions \
+        --nodrop \
+        --dump_database gorge
 
-echo "Uploading old measurements partitions to s3"
-aws s3 cp \
-    --storage-class STANDARD_IA \
-    --recursive \
-    ./partitions \
-    s3://${S3_BUCKET}/${S3_PREFIX}partitions_$(date +"%Y-%m-%d")/
+    echo "Uploading old measurements partitions to s3"
+    aws s3 cp \
+        --storage-class STANDARD_IA \
+        --recursive \
+        ./partitions \
+        s3://${S3_BUCKET}/${S3_PREFIX}partitions_$(date +"%Y-%m-%d")/
 
-# Second time we really drop them
-echo "Dumping and dropping old measurements partitions "
-dump_partition.py \
-    --schema archive \
-    --connection "dbname=gorge host=${PGHOST} user=${PGUSER} password=${PGPASSWORD}" \
-    --output ./partitions \
-    --nohashfile \
-    --dump_database gorge
+    # Second time we really drop them
+    echo "Dumping and dropping old measurements partitions "
+    dump_partition.py \
+        --schema archive \
+        --connection "dbname=gorge host=${PGHOST} user=${PGUSER} password=${PGPASSWORD}" \
+        --output ./partitions \
+        --nohashfile \
+        --dump_database gorge
+fi
 
 echo "Creating dump of wwguide database..."
 pg_dump -Fc --no-owner --no-privileges --no-password -f wwguide.bak wwguide
